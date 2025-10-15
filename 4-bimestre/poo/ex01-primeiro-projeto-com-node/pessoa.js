0cc1efeb68adf8b1fd59c27cd72f06cc3d5eb10c
João Guilherme Lins <162372169+Guilherm03@users.noreply.github.com>
2025-10-15 11:00:46 +0000

    update: pessoa.js corrigido

--- 4-bimestre/poo/ex01-primeiro-projeto-com-node/pessoa.js
+++ 4-bimestre/poo/ex01-primeiro-projeto-com-node/pessoa.js
@@ -2,10 +2,7 @@ function apresentarPessoa(nome, idade){
     return(`Meu nome é ${nome} e tenho ${idade} anos.`)
 }
 
-if (require.main === module) {
-  const nome = "João"
-  const idade = 16
-  console.log(apresentarPessoa(nome, idade))
-}
+const joao = (apresentarPessoa("João", 16))
+console.log(joao)
 
 module.exports = apresentarPessoa
