47d9b069c9423014be42cf8059abcc0ca67ec170
João Guilherme Lins <162372169+Guilherm03@users.noreply.github.com>
2025-11-11 19:52:57 +0000

    up

--- 4-bimestre/poo/projetos/projeto6-api-de-catalogo/src/routes/index.ts
+++ 4-bimestre/poo/projetos/projeto6-api-de-catalogo/src/routes/index.ts
@@ -1,16 +1,21 @@
-import { Router, Request, Response } from "express";
-import produtosRouter from "./produtos.js";
+import express from "express";
+import produtosRouter from "./produtos";
 
-const router = Router();
+const router = express.Router();
 
-router.get("/ping", (req: Request, res: Response) => {
+// Rota de produtos
+router.use("/produtos", produtosRouter);
+
+// Rota principal (nome e idade)
+router.get("/", (req, res) => {
+  let nome: string = "João Guilherme"; // altere para o seu nome
+  let idade: number = 16;              // e sua idade real
+  res.json({ nome, idade });
+});
+
+// Rota de teste
+router.get("/ping", (req, res) => {
   res.json({ pong: true });
 });
-
-router.get("/", (req: Request, res: Response) => {
-  res.json({ nome: "Everson", idade: 34 });
-});
-
-router.use("/produtos", produtosRouter);
 
 export default router;
