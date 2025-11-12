9266ffd9e8473a69688edf0fdc24a3ef06d4ff67
João Guilherme Lins <162372169+Guilherm03@users.noreply.github.com>
2025-11-12 10:57:21 +0000

    up

--- 4-bimestre/poo/projetos/projeto6-api-de-catalogo/src/routes/index.ts
+++ 4-bimestre/poo/projetos/projeto6-api-de-catalogo/src/routes/index.ts
@@ -2,20 +2,18 @@ import express from "express";
 import produtosRouter from "./produtos";
 
 const router = express.Router();
+router.use("/produtos", produtosRouter)
 
-// Rota de produtos
-router.use("/produtos", produtosRouter);
 
-// Rota principal (nome e idade)
 router.get("/", (req, res) => {
-  let nome: string = "João Guilherme"; // altere para o seu nome
-  let idade: number = 16;              // e sua idade real
-  res.json({ nome, idade });
-});
+  let nome: string = "Everson"
+  let idade: number = 34
+  res.json({ nome, idade })
+})
 
 // Rota de teste
 router.get("/ping", (req, res) => {
-  res.json({ pong: true });
-});
+  res.json({ pong: true })
+})
 
-export default router;
+export default router
--- 4-bimestre/poo/projetos/projeto6-api-de-catalogo/src/routes/produtos.ts
+++ 4-bimestre/poo/projetos/projeto6-api-de-catalogo/src/routes/produtos.ts
@@ -1,32 +1,32 @@
-import { Router, Request, Response } from "express";
+import { Router, Request, Response } from "express"
 
-const router = Router();
+const router = Router()
 
 let produtos = [
   { id: 1, nome: "Mouse Gamer", preco: 120 },
   { id: 2, nome: "Teclado Mecânico", preco: 350 },
   { id: 3, nome: "Monitor Full HD", preco: 900 },
-];
+]
 
 router.get("/", (req: Request, res: Response) => {
-  res.json(produtos);
-});
+  res.json(produtos)
+})
 
 router.get("/:id", (req: Request, res: Response) => {
-  const id = Number(req.params.id);
-  const produto = produtos.find((p) => p.id === id);
+  const id = Number(req.params.id)
+  const produto = produtos.find((p) => p.id === id)
 
   if (produto) {
-    res.json(produto);
+    res.json(produto)
   } else {
-    res.status(404).json({ erro: "Produto não encontrado" });
+    res.status(404).json({ erro: "Produto não encontrado" })
   }
 });
 
 router.post("/", (req: Request, res: Response) => {
-  const novoProduto = req.body;
-  produtos.push(novoProduto);
-  res.status(201).json(novoProduto);
-});
+  const novoProduto = req.body
+  produtos.push(novoProduto)
+  res.status(200).json(novoProduto)
+})
 
-export default router;
+export default router
